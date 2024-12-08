ed33cbe036fb052281f07adb67b9a1253750be6f
rrraine <lorraineequezada@gmail.com>
2024-12-08 11:52:00 +0800

    whywhy

---
+++ 4. Stack/Stack - Singly LL/linkedList.hpp
@@ -0,0 +1,110 @@
+#include <cstdlib>
+#include <iostream>
+#include "list.hpp"
+#include "node.hpp"
+using namespace std;
+
+class LinkedList : public List {
+	node* head;
+    node* tail;
+	int size;
+
+	public:
+	
+	LinkedList(){
+	    head = nullptr;
+        tail = nullptr;
+	    size = 0;
+	}
+    // TODO if necessary
+	int removeHead() {
+        if (!head) return 0;
+
+        node* temp = head;
+        int top = temp->elem;  
+        head->next = head;
+        delete temp;
+        size--;
+        return top; 
+	}
+
+    // TODO if necessary
+	void addTail(int num) {
+
+    }
+
+    // TODO if necessary
+    void addHead(int num) {
+        node* newNode = new node();
+        newNode->elem = num;
+        newNode->next = head;
+        head = newNode;
+        size++;
+    }
+
+    // TODO if necessary
+    
+    void add(int num) {
+        node* newNode = new node();
+        newNode->elem = num;
+
+        if (!head){
+            head = new node();
+            tail = new node();
+            newNode->next = nullptr;
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    // TODO add more methods as necessary
+
+    int _size() {
+    	return size;
+	}
+
+    int get(int pos) {
+        if (pos < 0 || pos >= size) {
+            return -1;
+        }
+
+        if (pos == 1) {
+            return head->elem;
+        }
+
+        node* curr = head;
+
+        for (int i = 1; i < pos; i++){
+            curr = curr->next;
+        }
+
+        return curr->elem;
+        
+    	//return 0;
+    }
+
+    int remove(int num) {
+        return 0;
+	}
+
+    void print() {
+    	node* curr = head;
+    	if (size == 0) {
+    		cout << "Empty" << endl;
+		}
+		else {
+	    	do {
+	    		cout << curr->elem;
+	    		if (curr->next) {
+	    			cout << " -> ";
+				} else {
+					cout << endl;
+				}
+	    		curr = curr->next;
+			} while (curr);
+		}
+    }
+};
---
+++ 4. Stack/Stack - Singly LL/singlyStack.hpp
@@ -0,0 +1,47 @@
+//TODO implement the class SinglyStack here
+// by inheriting the Stack class
+
+#include <iostream>
+#include "linkedlist.hpp"
+#include "stack.hpp"
+using namespace std;
+
+class SinglyStack : public Stack{
+
+    LinkedList* stack;
+    
+    public:
+    
+    SinglyStack() {
+        stack = new LinkedList();
+    }
+    
+    void push(int e){
+        stack->addHead(e);
+    }
+    
+    int top(){
+        if (stack->get(1) == -1) return 0;
+
+        return stack->get(1);
+    }
+    
+    int size() {
+        return stack->_size();
+    }
+    
+    int pop(){
+        return stack->removeHead();
+    
+    }
+    
+    bool isEmpty(){
+        return size() == 0;
+    }
+    
+    void print(){
+        stack->print();
+        return;
+    }
+    
+};
